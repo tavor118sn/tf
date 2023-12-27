4a10ecddad8f71d88352db139d7141b72bd60ece
Andrew Petriv <andrey.petriv@starnavi.io>
2023-12-27 16:26:53 +0200

    Init terraform

--- variables.tf
+++ variables.tf
@@ -1,15 +1,36 @@
-variable "GOOGLE_PROJECT" {
+# module "github_repository"
+variable "GITHUB_OWNER" {
   type        = string
-  default     = "glossy-precinct-406209"
-  description = "GCP project to use"
+  description = "GitHub owner repository to use"
 }
 
-variable "GOOGLE_REGION" {
+variable "GITHUB_TOKEN" {
   type        = string
-  description = "GCP region to use"
+  description = "GitHub personal access token"
 }
 
-variable "GKE_NUM_NODES" {
-  type        = number
-  description = "Number of nodes"
+variable "FLUX_GITHUB_REPO" {
+  type        = string
+  default     = "flux-gitops"
+  description = "Flux GitOps repository"
 }
+
+
+# # module "gke_cluster"
+# variable "GOOGLE_PROJECT" {
+#   type        = string
+#   default     = "glossy-precinct-406209"
+#   description = "GCP project to use"
+# }
+
+# variable "GOOGLE_REGION" {
+#   type        = string
+#   default     = "us-east1"
+#   description = "GCP region to use"
+# }
+
+# variable "GKE_NUM_NODES" {
+#   type        = number
+#   default     = 1
+#   description = "Number of nodes"
+# }
